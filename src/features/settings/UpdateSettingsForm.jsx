efe59a30981d441fa309697a0ac739b710ea5f63
radek <radek.glogowski3@gmail.com>
2024-03-17 13:45:06 +0100

    feat: update settings

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -3,6 +3,7 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 import Spinner from "../../ui/Spinner";
 import { useSettings } from "./useSettings";
+import { useUpdateSetting } from "./useUpdateSetting";
 
 function UpdateSettingsForm() {
   const {
@@ -14,8 +15,16 @@ function UpdateSettingsForm() {
       breakfastPrice,
     } = {},
   } = useSettings();
+  const { isUpdating, updateSetting } = useUpdateSetting();
 
   if (isLoading) return <Spinner />;
+
+  function handleUpdate(e, field) {
+    const { value } = e.target;
+    if (!value) return;
+
+    updateSetting({ [field]: value });
+  }
 
   return (
     <Form>
@@ -24,7 +33,8 @@ function UpdateSettingsForm() {
           type="number"
           id="min-nights"
           defaultValue={minBookingLength}
-          disabled={isLoading}
+          disabled={isUpdating}
+          onBlur={(e) => handleUpdate(e, "minBookingLength")}
         />
       </FormRow>
       <FormRow label="Maximum nights/booking">
@@ -32,7 +42,7 @@ function UpdateSettingsForm() {
           type="number"
           id="max-nights"
           defaultValue={maxBookingLength}
-          disabled={isLoading}
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow label="Maximum guests/booking">
@@ -40,7 +50,7 @@ function UpdateSettingsForm() {
           type="number"
           id="max-guests"
           defaultValue={maxGuestsPerBooking}
-          disabled={isLoading}
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow label="Breakfast price">
@@ -48,7 +58,7 @@ function UpdateSettingsForm() {
           type="number"
           id="breakfast-price"
           defaultValue={breakfastPrice}
-          disabled={isLoading}
+          disabled={isUpdating}
         />
       </FormRow>
     </Form>
--- src/features/settings/useUpdateSetting.js
+++ src/features/settings/useUpdateSetting.js
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 
 import { updateSetting as updatesettingApi } from "../../services/apiSettings";
 
-export function useEditSetting() {
+export function useUpdateSetting() {
   const queryClient = useQueryClient();
 
   const { mutate: updateSetting, isLoading: idUpdating } = useMutation({
